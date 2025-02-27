7af5c3e54a35f073dc9f1f430ce82b3b3d432237
Morgan Muthee <125727893+23MorganDev@users.noreply.github.com>
2025-02-28 00:14:18 +0300

    Update configPaths.js

--- house-website/src/components/Paths/configPaths.js
+++ house-website/src/components/Paths/configPaths.js
@@ -2,8 +2,8 @@ const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || "http://localh
 
 const configPath = {
     BACKEND_BASE_URL,
-    ENDPOINTS:{
-        BOOKING: "/bookings/create"
+    ENDPOINTS: {
+        BOOKING: `${BACKEND_BASE_URL}/backend/bookings/create` 
     }
-}
+};
 export default configPath;
